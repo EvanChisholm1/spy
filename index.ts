0da8dc54727eebd3dc0599f7aa6296071ea937a4
Evan Chisholm <elchisholm248@gmail.com>
2023-09-02 23:16:30 -0400

    added little api call

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-console.log("Hello!");
-
-setTimeout(() => {
+setTimeout(async () => {
     console.log(document.body.innerText);
+    const res = await fetch("http://localhost:8080", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+            text: document.body.innerText,
+        }),
+    });
 }, 1000);
